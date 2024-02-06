81baafd5b7d3755303eb2e07f9f0514a80b87d1b
Kaio Aime Garcia <kaiogarcia@employer.com.br>
2024-02-05 22:56:51 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=D:\Meus Documentos\OneDrive\Área de Trabalho\nlw 2024 C#\nlw-expert-c-sharp\leilaoDbNLW.db");
+    }
+}
+ 
